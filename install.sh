b035f2ff4ab88d6392325d948b9442966e043b5e
Timothy Mamo <github@timothymamo.com>
2025-06-30 15:15:54 +0200

    Will install as tim after first boot

--- install.sh
+++ install.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
 
 # Change directory to $[HOME]
-cd $[HOME]
+pushd ${HOME}
 
-# Create a key for user tim
-ssh-keygen -a 100 -t ed25519 -f ~/.ssh/id_ed25519 -C "${USER}@${HOSTNAME}"
-
-# Log into Github and copy the public key of the generated ssh file into Settings > SSH and GPG keys > New SSH Key.
-cat /home/${USER}/.ssh/id_ed25519.pub
-
-#Install Homebrew
+# Install Homebrew
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 sudo apt-get install build-essential -y
 
 # Create a git directory and clone this repo
-mkdir git && cd git
-git clone git@github.com:timothymamo/dietpi-post-install.git
+mkdir git
+git clone https://github.com/timothymamo/dietpi-post-install.git "${HOME}/git/dietpi-post-install/"
 
 # Set brew in shell
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 # Create symlinks for all files apart from the .git directory
-ln -s /home/${USER}/git/dietpi-post-install/* .
-ln -s /home/${USER}/git/dietpi-post-install/.* .
-rm -rf .git
+ln -s ${HOME}/git/dietpi-post-install/* ${HOME}
+ln -s ${HOME}/git/dietpi-post-install/.* ${HOME}
+
+# Remove symlinks for .git and .env inbthe ${HOME} directory so any changes within ${HOME} don't get pushed to the repo
+rm -rf ${HOME}/.git ${HOME}/docker-compose/.env
+
+# Copy the .env file to the ${HOME} directory - this will be modified later but is needed to run docker compose
+cp -r ${HOME}/git/dietpi-post-install/docker-compose/.env ${HOME}/docker-compose/.env
 
 # Install everything with the Brewfile
 brew bundle
@@ -32,17 +31,20 @@ brew install --cask font-fira-code-nerd-font
 
 # Set zsh as the default shell for the ${USER}
 command -v zsh | sudo tee -a /etc/shells
-sudo chsh -s "$(command -v zsh)" "${USER}"
+sudo chsh -s "$(command -v zsh)" ${USER}
 
 # Disable root ssh login
-sed -i '/#PermitRootLogin prohibit-password/c\PermitRootLogin no' /etc/ssh/sshd_config
+sudo sed -i '/#PermitRootLogin prohibit-password/c\PermitRootLogin no' /etc/ssh/sshd_config
 
 # Restart sshd
 sudo systemctl restart sshd
 
 # Setup Docker to have the appropriate permissions
 sudo usermod -aG docker ${USER}
-newgrp docker
 sudo systemctl enable docker
-cd docker-compose
+pushd ${HOME}/docker-compose
 docker compose up -d
+
+# Wait for 2 minutes before rebooting the system
+sleep 120
+sudo poweroff --reboot
