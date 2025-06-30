343a2c0b16c361325f493f1becc39d5c5c0aec68
Timothy Mamo <github@timothymamo.com>
2025-06-30 14:27:51 +0200

    Testing

--- Automation_Custom_Script.sh
+++ Automation_Custom_Script.sh
@@ -9,9 +9,12 @@ chown -R tim:tim /home/tim/.ssh
 
 echo 'tim ALL=(ALL:ALL) ALL' >> /etc/sudoers
 
-sudo -u tim bash<<_
+HOME_TIM='/home/tim'
+
+su tim
+
 # Change directory to $[HOME]
-pushd ${HOME}
+pushd ${HOME_TIM}
 
 # Install Homebrew
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -19,20 +22,20 @@ sudo apt-get install build-essential -y
 
 # Create a git directory and clone this repo
 mkdir git
-git clone https://github.com/timothymamo/dietpi-post-install.git "${HOME}/git/dietpi-post-install/"
+git clone https://github.com/timothymamo/dietpi-post-install.git "${HOME_TIM}/git/dietpi-post-install/"
 
 # Set brew in shell
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 # Create symlinks for all files apart from the .git directory
-ln -s ${HOME}/git/dietpi-post-install/* ${HOME}
-ln -s ${HOME}/git/dietpi-post-install/.* ${HOME}
+ln -s ${HOME_TIM}/git/dietpi-post-install/* ${HOME_TIM}
+ln -s ${HOME_TIM}/git/dietpi-post-install/.* ${HOME_TIM}
 
-# Remove symlinks for .git and .env inbthe ${HOME} directory so any changes within ${HOME} don't get pushed to the repo
-rm -rf ${HOME}/.git ${HOME}/docker-compose/.env
+# Remove symlinks for .git and .env inbthe ${HOME_TIM} directory so any changes within ${HOME_TIM} don't get pushed to the repo
+rm -rf ${HOME_TIM}/.git ${HOME_TIM}/docker-compose/.env
 
-# Copy the .env file to the ${HOME} directory - this will be modified later but is needed to run docker compose
-cp -r ${HOME}/git/dietpi-post-install/docker-compose/.env ${HOME}/docker-compose/.env
+# Copy the .env file to the ${HOME_TIM} directory - this will be modified later but is needed to run docker compose
+cp -r ${HOME_TIM}/git/dietpi-post-install/docker-compose/.env ${HOME_TIM}/docker-compose/.env
 
 # Install everything with the Brewfile
 brew bundle
@@ -52,10 +55,9 @@ sudo systemctl restart sshd
 # Setup Docker to have the appropriate permissions
 sudo usermod -aG docker ${USER}
 sudo systemctl enable docker
-pushd ${HOME}/docker-compose
+pushd ${HOME_TIM}/docker-compose
 docker compose up -d
 
 # Wait for 2 minutes before rebooting the system
 sleep 120
 sudo poweroff --reboot
-_
